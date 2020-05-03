ea548c855fb0052baf22f433229786cea799f554
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-05-03 22:49:19 +0800

    Eksekusi Class,atribut.behavior dan object

---
+++ src/elektronik.java
@@ -0,0 +1,17 @@
+public class elektronik {
+    public static void main(String[] args) {
+        Toko object;
+        object = new Toko();
+        object.pemilik= "Alpiah Hasanah";
+        object.alamat="Jl.Pramuka No.35";
+        object.pegawai= "12 orang";
+        object.telpon= "086787790690";
+        object.Email= "alpiahhasanah611@gmail.com";
+
+        object.pengenalan();
+        object.kipasAngin();
+        object.kulkas();
+        object.telivisi();
+
+    }
+}
